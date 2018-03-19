cc49309dc0c018ab3237bc70be6ad6992d1a01ee
Tyler Westlie <tyler.westlie@gmail.com>
2018-03-19 15:45:04 -0600

    iteration 1 testing

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,6 +1,19 @@
+require './lib/node'
 require 'pry'
 
 class LinkedList
+  attr_reader :head
+
   def initialize
+    @head = nil
+  end
+
+  def append
+  end
+
+  def count
+  end
+
+  def to_string
   end
 end
--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -1,12 +1,24 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/linked_list.rb'
+require './lib/linked_list'
 require 'pry'
 
 class TestLinkedList < Minitest::Test
+
   def test_exist
     list = LinkedList.new
 
     assert_instance_of LinkedList, list
   end
+
+  def test_head
+    link = LinkedList.new
+
+    assert_nil link.head
+  end
+
+  def test_append
+    link = LinkedList.new
+  end
+
 end
